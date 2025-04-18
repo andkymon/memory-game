41299bde3e7f63fe5601583ea713873af36bf43f
andkymon <andkymon@gmail.com>
2025-04-18 19:51:46 +0800

    Implement score and best Score counters

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,38 @@ import { useState } from 'react'
 
 function App() {
   const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   const onCardClick = () => {
     setScore(score + 1);
+    if (score + 1 > bestScore) setBestScore(bestScore + 1);
+  }
+
+  // Just a temporary function to check if best score works as intended
+  const onCardClick2 = () => {
+    setScore(0);
   }
 
   return (
     <>
-      <Header score={score} />
+      <Header score={score} bestScore={bestScore} />
       <main>
         <Card onClick={onCardClick} />
-        <Card onClick={onCardClick} />
+        <Card onClick={onCardClick2} />
         <Card onClick={onCardClick} />
       </main>
     </>
   )
 }
 
-function Header({ score }) {
+function Header({ score, bestScore }) {
   return (
     <header>
       <h1>Memory Game: Cat Edition</h1>
         <div className='instructions'>One click per kitty, repeat and you’re beat! 🐱</div>
         <div className='score-wrapper'>
-          <h2>Score: {score.toString()}</h2>
-          <h2>Best Score:</h2>
+          <h2>Score: {score}</h2>
+          <h2>Best Score: {bestScore}</h2>
         </div>
     </header>
   )
