6c1f7cb5e9a6cdee27cfc7e5a5da8c04a0865de9
andkymon <andkymon@gmail.com>
2025-04-18 17:40:05 +0800

    Add and style card components in main
    Add and style card components in main

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <>
       <Header />
+      <main>
+        <Card />
+        <Card />
+        <Card />
+      </main>
     </>
   )
 }
@@ -12,12 +17,21 @@ function Header() {
   return (
     <header>
       <h1>Memory Game: Cat Edition</h1>
-        <div className='instructions'>One click per kitty—repeat and you’re beat! 🐾</div>
+        <div className='instructions'>One click per kitty, repeat and you’re beat! 🐱</div>
         <div className='score-wrapper'>
           <h2>Score:</h2>
           <h2>Best Score:</h2>
         </div>
     </header>
+  )
+}
+
+function Card() {
+  return (
+    <button className='card'>
+      <div className='img'></div>
+      <h3>Cat Name</h3>
+    </button>
   )
 }
 
