7701fd0761b206a2ed5ebfbba0a2e50dce5b1448
andkymon <andkymon@gmail.com>
2025-04-18 17:17:29 +0800

    Structure and style Header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import './App.css'
 function App() {
   return (
     <>
+      <Header />
     </>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Memory Game: Cat Edition</h1>
+        <div className='instructions'>One click per kitty—repeat and you’re beat! 🐾</div>
+        <div className='score-wrapper'>
+          <h2>Score:</h2>
+          <h2>Best Score:</h2>
+        </div>
+    </header>
   )
 }
 
