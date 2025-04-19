d9236c767da1dac04cbcb34dfd9526e3a49840da
andkymon <andkymon@gmail.com>
2025-04-19 14:28:40 +0800

    Fetch data from cat API

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import shuffle from './utils/shuffle';
 
 function App() {
-  const [catIDs, setCatIDs] = useState([
-      "pers",
-      "siam",
-      "beng",
-      "sphy",
-      "mcoo",
-      "rblu",
-      "ragd",
-      "sfol",
-      "buri",
-      "abys",
-      "bsho",
-      "orie",
-    ]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [catData, setCatData] = useState([]);
+
+  useEffect(() => {
+    let isActive = true;
+    const temp = [];
+
+    const catIDs = ["pers","siam","beng","sphy","mcoo","rblu","ragd","sfol","buri","abys","bsho","orie"];
+    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=`;
+    const api_key = "live_eSPdZgaQ4sJiUjA4Mw7mhT1idVDPsV7lS67UPtJwp3Dko493j4QxyNfcRbxtuEQS"
+
+    const fetches = catIDs.map(catID => fetch(url + catID, {headers: {'x-api-key': api_key}})
+    .then(res => res.json())
+    .then((data) => {
+      if (isActive) temp.push({ id: data[0].breeds[0].id, name: data[0].breeds[0].name, img: data[0].url })
+    }));
+
+    Promise.all(fetches).then(() => setCatData(temp));
+    
+    return () => {
+      isActive = false;
+    };
+  }, []);
 
   const onCardClick = (e) => {
     setScore(score + 1);
     if (score + 1 > bestScore) setBestScore(bestScore + 1);
-    setCatIDs(shuffle(catIDs));
+    shuffle(catData);
     e.currentTarget.blur(); // removing button focus on click
   };
 
+  //TODO: If cat array empty, display spinner
   return (
     <>
       <Header score={score} bestScore={bestScore} />
       <main>
-        {catIDs.map(entry => <Card key={entry} id={entry} onClick={onCardClick} />)}
+          {catData.length > 1 && catData.map(data => <Card key={data.id} name={data.name} img={data.img} onClick={onCardClick} />)}
       </main>
     </>
   );
@@ -50,11 +59,11 @@ function Header({ score, bestScore }) {
   )
 }
 
-function Card({ id, onClick }) {
+function Card({ name, img, onClick }) {
   return (
     <button className='card' onClick={onClick}>
-      <div className='img'></div>
-      <h3>{id}</h3>
+      <img className='img' src={img} />
+      <h3>{name}</h3>
     </button>
   )
 }
